02edc0d9417db7aaab83bb53632ad2c2b9b67812
hique023 <pedroviski10@hotmail.com>
2020-10-15 22:10:09 -0300

    Melhorado método renderTodos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,26 +2,36 @@ let listaTodo = document.querySelector('#listaTodo')
 let inputElement = document.querySelector('#inputElement')
 let buttonElement = document.querySelector('#btnElement')
 
-
 let todos = JSON.parse(localStorage.getItem('list_todos')) || []
 
 function renderTodos() {
     listaTodo.innerHTML = ''
     for (todo of todos){
-        let todoElement = document.createElement('li')
-        let textTodo = document.createTextNode(todo)
-        let excluir = document.createElement('button')
-        let textExcluir = document.createTextNode('Excluir')
+        let todoElement = criarElemento('li')
+        let textTodo = criarText(todo)
+        let excluir = criarElemento('button')
+        let textExcluir = criarText('Excluir')
         
-        todoElement.appendChild(textTodo)
-        listaTodo.appendChild(todoElement)
-        
-        excluir.appendChild(textExcluir)
-        todoElement.appendChild(excluir)
+        addChild(todoElement, textTodo)
+        addChild(listaTodo, todoElement)
+        addChild(excluir, textExcluir)
+        addChild(todoElement, excluir)
         
         let pos = todos.indexOf(todo)
         excluir.setAttribute('onclick', 'excluirTodo(' + pos + ')')
     }
+}
+
+function criarElemento(element){
+    return document.createElement(element)
+}
+
+function criarText(text){
+    return document.createTextNode(text)
+}
+
+function addChild(principal, filho){
+    return principal.appendChild(filho)
 }
 
 renderTodos()
